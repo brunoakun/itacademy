5e333e0e2f8e8e700747bad7aa87b2506fac32de
Bruno Barange <bruno.akun@gmail.com>
2022-04-04 11:17:29 +0200

    m8 v2

---
+++ m8/ordenador.js
@@ -0,0 +1,36 @@
+class Ordenador {
+    // atributos de clase
+    constructor(marca, modelo, cpu, ram, hd) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.hd = hd;
+    }
+
+    // Métodos de clase
+    getMarca() {
+        return this.marca;
+    }
+    getModelo() {
+        return this.modelo;
+    }
+    getCpu() {
+        return this.cpu;
+    }
+
+    getNombre(nombre) {
+        return ("En aquests moments s'està executant: " + nombre);
+    }
+
+    getDescipcion() {
+        let resultado = "";
+        resultado += ' marca:' + this.marca + "<br>";
+        resultado += ' modelo:' + this.modelo + "<br>";
+        resultado += ' cpu:' + this.cpu + "<br>";
+        resultado += ' ram:' + this.ram + "<br>";
+        resultado += ' hd:' + this.hd
+        return (resultado);
+    }
+
+}
