8f394e9663394da739245dc036d4a8e378b45e59
Bruno Barange <bruno.akun@gmail.com>
2022-04-05 15:44:36 +0200

    rev

--- m8/main.js
+++ m8/main.js
@@ -10,6 +10,6 @@ let ordenador1 = new Ordenador(datosPc1.marca, datosPc1.modelo, datosPc1.cpu, da
 let ordenador2 = new Ordenador("marca2", datosPc1.modelo, datosPc1.cpu, datosPc1.ram);
 let divResultado = document.getElementById("divResultado");
 
-alert(`marca del ordenador1 es ${ordenador1._marca}`);
+alert(`marca del ordenador1 es ${ordenador1._marca}`);  // Usamos el getter
 divResultado.innerHTML = '<h1>ordenador1</h1>' + ordenador1.getDescipcion();
 divResultado2.innerHTML = '<h1>ordenador2</h1>' + ordenador2.getDescipcion();
