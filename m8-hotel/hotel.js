32746783916114bbaa108ab589dbb5141a88504b
Bruno Barange <bruno.akun@gmail.com>
2022-04-18 11:43:47 +0200

    hoteles

---
+++ m8-hotel/hotel.js
@@ -0,0 +1,40 @@
+class Hotel {
+
+    // ATRIBUTOS de la clase hotel
+    constructor(nombre, habitaciones, plantas, superficie) {
+        this._nombre = nombre;
+        this._habitaciones = habitaciones;
+        this._plantas = plantas;
+        this._superficie = superficie;
+    }
+
+    // GETTERS Y SETTERS
+    get nombre() { return this._nombre; }
+    get habitaciones() { return this._habitaciones; }
+    get plantas() { return this._plantas; }
+    get superficie() { return this._superficie; }
+
+    set nombre(val) { this._nombre = val; }
+    set habitaciones(val) { this._habitaciones = val; }
+    set plantas(val) { this._plantas = val; }
+    set superficie(val) { this._superficie = val; }
+
+
+    // METODOS DE LA CLASE 
+
+    getNombre(nombre) {
+        return ("En aquests moments s'està executant: " + nombre);
+    }
+
+    getDescipcion() {
+        let resultado = "<ul>";
+        resultado += '<li> marca: ' + this.marca + "</li>";
+        resultado += '<li> modelo: ' + this.modelo + "</li>";
+        resultado += '<li> cpu: ' + this.cpu + "</li>";
+        resultado += '<li> ram: ' + this.ram + "</li>";
+        resultado += '<li> hd: ' + this.hd + "</li>";
+        resultado += '</ul>';
+        return (resultado);
+    }
+
+}
---
+++ m8-hotel/main.js
@@ -0,0 +1,73 @@
+const arrayHoteles = [];
+
+function crearHotel() {
+    let hotelData = {
+        nombre: prompt('nombre: '),
+        habitaciones: prompt('habitaciones: '),
+        plantas: prompt('plantas: '),
+        superficie: prompt('superficie: ')
+    }
+    const hotel = new Hotel(hotelData.nombre, hotelData.habitaciones, hotelData.plantas, hotelData.superficie);
+    arrayHoteles.push(hotel);
+   // alert(`Creado el Hotel ${hotelData.nombre} dentro del array arrayHoteles[]`);
+    muestraTodos();
+}
+
+
+function datosHotel() {
+    let nombre = prompt('nombre: ');
+    veureHotel(nombre);
+}
+
+
+function veureHotel(nombre) {
+    let result = '';
+    if (arrayHoteles.length == 0) {
+        result = "Hotel " + nombre + " <b>NO existe</b>";
+        result += "<hr>";
+        divResultado.innerHTML = result;
+        return;
+    }
+    for (i = 0; i < arrayHoteles.length; i++) {
+        if (arrayHoteles[i].nombre == nombre) {
+            result += 'Hotel nº ' + i + "<br>";
+            result += '<b>Nombre: ' + arrayHoteles[i].nombre + "</b><br>";
+            result += 'Habitaciones: ' + arrayHoteles[i].habitaciones + "<br>";
+            result += 'Plantas: ' + arrayHoteles[i].plantas + "<br>";
+            result += 'Superficie: ' + arrayHoteles[i].superficie + "<br>";
+        }
+    }
+    if (result == '') result = "Hotel " + nombre + " <b>NO existe</b>";
+    divResultado.innerHTML = result;
+}
+
+
+function donarDeBaixaHotel() {
+    let nombre = prompt('Nombre a BORRAR: ');
+    let result = '';
+    let borrado = false;
+    for (i = 0; i < arrayHoteles.length; i++) {
+        if (arrayHoteles[i].nombre == nombre) {
+            arrayHoteles.splice(i, 1);
+            borrado = true;
+        }
+    }
+
+    if (borrado) {
+        result += `Borrado el Hotel ${nombre} dentro del array arrayHoteles[]`;
+    } else {
+        result += `Hotel ${nombre} NO existe `;
+    }
+    divResultado.innerHTML = result;
+    muestraTodos();
+}
+
+
+function muestraTodos() {
+    let result = '';
+    if (!arrayHoteles) return;
+    for (i = 0; i < arrayHoteles.length; i++) {
+        result += i + " - " + arrayHoteles[i].nombre + " <br>";
+    }
+    divResultado2.innerHTML = result;
+}
