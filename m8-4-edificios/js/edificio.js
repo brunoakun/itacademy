cfb9ee971644cfdd6f235e98fe8b3562c8532b9a
Bruno Barange <bruno.akun@gmail.com>
2022-04-26 17:23:34 +0200

    rev 5

--- m8-4-edificios/js/edificio.js
+++ m8-4-edificios/js/edificio.js
@@ -9,13 +9,13 @@ class Edificio {
 
     //GETTERS & SETTERS
 
-    get nombre() {return this._nombre;}
-    get plantas() {return this._plantas;}
-    get superficie() {return this._superficie;}
-     
-    set superficie(superficie) {this._superficie = superficie;}     
-    set plantas(plantas) {this._plantas = plantas;}     
-    set nombre(nombre) {this._nombre = nombre;}
+    get nombre() { return this._nombre; }
+    get plantas() { return this._plantas; }
+    get superficie() { return this._superficie; }
+
+    set superficie(superficie) { this._superficie = superficie; }
+    set plantas(plantas) { this._plantas = plantas; }
+    set nombre(nombre) { this._nombre = nombre; }
 
     // METODOS
 
@@ -23,14 +23,19 @@ class Edificio {
 
 class Hospital extends Edificio {
 
+    // PROPIEDADES
     constructor(pacientes) {
         this._pacientes = pacientes;
 
-         // Hereda de la clase superior
+        // Hereda de la clase superior
         super(nombre);
         super(plantas);
         super(superficie);
     }
+
+    // GETTERS & SETTERS
+    get pacientes() {return this._pacientes;}     
+    set pacientes(pacientes) {this._pacientes = pacientes;}
 
     // METODOS
     repartirDinar() {
